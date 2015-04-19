b4420360c17e50f1821b98ad86a71bc44dd835c5
y-uti <y-uti@users.noreply.github.com>
2015-04-19 21:31:41 +0900

    Add optimal solver for 2nd row, and encode result

--- secondrow.php
+++ secondrow.php
@@ -2,14 +2,24 @@
 
 function main()
 {
-    $result = str_repeat(' ', 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1);
-    $result[0] = 'S';
-    $board = array_fill(0, 4, array_fill(0, 4, -1));
+    $result = str_repeat(' ', 12 * 11 * 10 * 9 * 8);
 
-    $queue = [0];
+    $queue = [];
+    $board = array_fill(0, 4, array_fill(0, 4, -1));
+    $board[1] = [5, 6, 7, 8];
+    foreach (range(2, 3) as $sy) {
+        foreach (range(0, 3) as $sx) {
+            $board[$sy][$sx] = 0;
+            $encnum = encode($board);
+            $result[$encnum] = 'S';
+            $queue[] = $encnum;
+            $board[$sy][$sx] = -1;
+        }
+    }
+
     while ($queue) {
         $encnum = array_shift($queue);
-        decode($encnum, $board);
+        $board = decode($encnum);
         $p = locationOf(0, $board);
         tryNext($p, [0, -1], 'D', $board, $queue, $result);
         tryNext($p, [-1, 0], 'R', $board, $queue, $result);
@@ -24,7 +34,7 @@ function tryNext($curr, $d, $c, $board, &$queue, &$result)
 {
     list($x, $y) = $curr;
     list($dx, $dy) = $d;
-    if (0 <= $x + $dx && $x + $dx <= 3 && 2 <= $y + $dy && $y + $dy <= 3) {
+    if (0 <= $x + $dx && $x + $dx <= 3 && 1 <= $y + $dy && $y + $dy <= 3) {
         $next = step($curr, $d, $board);
         $nenc = encode($next);
         if ($result[$nenc] == ' ') {
@@ -34,53 +44,42 @@ function tryNext($curr, $d, $c, $board, &$queue, &$result)
     }
 }
 
-function test()
-{
-    $board = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [10, 14, 13, 9],
-        [15, 0, 12, 11],
-    ];
-
-    $encnum = encode($board);
-    echo $encnum . "\n";
-
-    $board2 = array_fill(0, 4, array_fill(0, 4, 0));
-    decode($encnum, $board2);
-
-    echo json_encode($board2) . "\n";
-}
-
 function encode($board)
 {
+    $numbers = [5, 6, 7, 8, 0];
+
     $encnum = 0;
-    $numbers = [9, 10, 11, 12, 13, 14, 15, 0];
+    $cells = range(4, 15);
     $scale = 1;
-    foreach (range(2, 3) as $r) {
-        foreach (range(0, 3) as $c) {
-            $n = $board[$r][$c];
-            $i = array_search($n, $numbers);
-            $encnum += $i * $scale;
-            $scale *= count($numbers);
-            array_splice($numbers, $i, 1);
-        }
+    foreach ($numbers as $n) {
+        list($x, $y) = locationOf($n, $board);
+        $c = $y * 4 + $x;
+        $i = array_search($c, $cells);
+        $encnum += $i * $scale;
+        $scale *= count($cells);
+        array_splice($cells, $i, 1);
     }
+
     return $encnum;
 }
 
-function decode($encnum, &$board)
+function decode($encnum)
 {
-    $numbers = [9, 10, 11, 12, 13, 14, 15, 0];
-    foreach (range(2, 3) as $r) {
-        foreach (range(0, 3) as $c) {
-            $i = $encnum % count($numbers);
-            $n = $numbers[$i];
-            $board[$r][$c] = $n;
-            $encnum = ($encnum - $i) / count($numbers);
-            array_splice($numbers, $i, 1);
-        }
+    $numbers = [5, 6, 7, 8, 0];
+
+    $board = array_fill(0, 4, array_fill(0, 4, -1));
+    $cells = range(4, 15);
+    foreach ($numbers as $n) {
+        $i = $encnum % count($cells);
+        $c = $cells[$i];
+        $x = $c % 4;
+        $y = ($c - $x) / 4;
+        $board[$y][$x] = $n;
+        $encnum = ($encnum - $i) / count($cells);
+        array_splice($cells, $i, 1);
     }
+
+    return $board;
 }
 
 function locationOf($n, $board)
