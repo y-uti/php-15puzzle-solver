f1e39bcaea4410f5e65b78008b8354535c5ed329
y-uti <y-uti@users.noreply.github.com>
2015-05-15 20:48:05 +0900

    Refine implementation

--- secondrow.php
+++ secondrow.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/gendata.php';
+
 function main()
 {
     $result = str_repeat(' ', 12 * 11 * 10 * 9 * 8);
@@ -80,50 +82,6 @@ function decode($encnum)
     }
 
     return $board;
-}
-
-function locationOf($n, $board)
-{
-    foreach ($board as $y => $r) {
-        foreach ($r as $x => $i) {
-            if ($i == $n) {
-                return array($x, $y);
-            }
-        }
-    }
-}
-
-function step($p, $d, $board)
-{
-    list($x, $y) = $p;
-    list($dx, $dy) = $d;
-    $n = $board[$y + $dy][$x + $dx];
-    $board[$y][$x] = $n;
-    $board[$y + $dy][$x + $dx] = 0;
-
-    return $board;
-}
-
-function packString($str)
-{
-    $packed = str_repeat(chr(0), strlen($str) / 4);
-    for ($i = 0; $i < strlen($str); $i += 4) {
-        $n0 = strnum($str[$i + 0]);
-        $n1 = strnum($str[$i + 1]);
-        $n2 = strnum($str[$i + 2]);
-        $n3 = strnum($str[$i + 3]);
-        $n = $n0 + $n1 * 4 + $n2 * 16 + $n3 * 64;
-        $packed[$i / 4] = chr($n);
-    }
-
-    return $packed;
-}
-
-function strnum($str)
-{
-    $table = ['U', 'L', 'R', 'D'];
-    $n = array_search($str, $table);
-    return $n !== false ? $n : 0;
 }
 
 main();
